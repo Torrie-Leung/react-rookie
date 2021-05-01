69c0e9f7bccb7d8efa023bb24ee00fae722e476b
TorrieL <hmminicity2@163.com>
2021-05-01 11:29:51 +0800

    onClick event

--- chimp-blog/src/Home/Home.js
+++ chimp-blog/src/Home/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log('hey');
+  }
+  const handleClickName = (name) => {
+    console.log('hey'+name);
+  }
   return ( 
     <div className="home">
       <h2>HomePage</h2>
+      <button onClick = {() => {
+        handleClickName('Yoshi')
+      }}>Click me</button>
     </div>
    );
 }
