dd78582d81db237b6979d13e321838055f88529b
TorrieL <hmminicity2@163.com>
2021-05-01 12:14:34 +0800

    react useState

--- chimp-blog/src/Home/Home.js
+++ chimp-blog/src/Home/Home.js
@@ -1,15 +1,26 @@
+import { useState } from 'react';
+
 const Home = () => {
+  const [name, setName] = useState('mario');
+  const [level,setLevel] = useState(2300)
   const handleClick = (e) => {
     console.log(e);
   }
   const handleClickName = (name,e) => {
     console.log('hey'+name, e.target);
   }
+  const changeName = () => {
+    setName('Luigi')
+    setLevel(4600)
+
+  }
   return ( 
     <div className="home">
       <h2>HomePage</h2>
       <button onClick ={handleClick}>hey event</button>
       <button onClick = {(e) => handleClickName('Yoshi',e)}>Click me show Name</button>
+
+      <p onClick={changeName}>{name} is level {level}</p>
     </div>
    );
 }
