3698921a01b63b031147b0e5d3d12091444c0c2d
TorrieL <hmminicity2@163.com>
2021-05-01 12:46:26 +0800

    map(xx => ())

--- chimp-blog/src/Home/Home.js
+++ chimp-blog/src/Home/Home.js
@@ -1,26 +1,19 @@
 import { useState } from 'react';
 
 const Home = () => {
-  const [name, setName] = useState('mario');
-  const [level,setLevel] = useState(2300)
-  const handleClick = (e) => {
-    console.log(e);
-  }
-  const handleClickName = (name,e) => {
-    console.log('hey'+name, e.target);
-  }
-  const changeName = () => {
-    setName('Luigi')
-    setLevel(4600)
-
-  }
+  const [blogs] = useState([
+    {title: 'Data Structure from craddle to grave', body:'Lets start from Linked list', author:'CodingChimp',id:1},
+    {title: 'Be a real trouble maker', body:'sharp tounged but golden hearted', author:'CodingChimp',id:2},
+  ]);
+  
   return ( 
     <div className="home">
-      <h2>HomePage</h2>
-      <button onClick ={handleClick}>hey event</button>
-      <button onClick = {(e) => handleClickName('Yoshi',e)}>Click me show Name</button>
-
-      <p onClick={changeName}>{name} is level {level}</p>
+      {blogs.map(blog => (
+        <div className="blogs-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
     </div>
    );
 }
