d8c29f26fc19f7be29bb1febef7b3788fe8d8074
TorrieL <hmminicity2@163.com>
2021-05-01 20:37:09 +0800

    react filter

--- chimp-blog/src/BlogLists.js
+++ chimp-blog/src/BlogLists.js
@@ -1,10 +1,10 @@
-const BlogList = (props) =>{
-  const blogs = props.blogs;
-  console.log(props);
+const BlogList = ({ blogs,title }) =>{
   return (
     <div className="blog-list">
+    <p>{title} area</p>
       {blogs.map(blog => (
         <div className="blogs-preview" key={blog.id}>
+          
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
         </div>
--- chimp-blog/src/Home/Home.js
+++ chimp-blog/src/Home/Home.js
@@ -5,11 +5,13 @@ const Home = () => {
   const [blogs] = useState([
     {title: 'Data Structure from craddle to grave', body:'Lets start from Linked list', author:'CodingChimp',id:1},
     {title: 'Be a real trouble maker', body:'sharp tounged but golden hearted', author:'CodingChimp',id:2},
+    {title: 'My treasure', body:'How to collect all the stamps', author:'Yoshi',id:3},
   ]);
   
   return ( 
     <div className="home">
-      <BlogList blogs={blogs}/>
+      <BlogList blogs={blogs.filter(blog=> blog.author == 'CodingChimp')} title="CoolChimps's"/>
+      <BlogList blogs={blogs.filter(blog=> blog.author == 'Yoshi')} title="Yoshi's"/>
     </div>
    );
 }
