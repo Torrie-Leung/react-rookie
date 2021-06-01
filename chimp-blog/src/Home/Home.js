ca479458ff1bc71df30aa04a16cdbccba9b8350a
TorrieL <hmminicity2@163.com>
2021-06-01 20:19:46 +0800

    Fetching Data with useEffect

--- chimp-blog/src/Home/Home.js
+++ chimp-blog/src/Home/Home.js
@@ -2,11 +2,7 @@ import { useState,useEffect } from 'react';
 import BlogList from '../BlogLists'
 
 const Home = () => {
-  const [blogs,setBlogs] = useState([
-    {title: 'Data Structure from craddle to grave', body:'Lets start from Linked list', author:'CodingChimp',id:1},
-    {title: 'Be a real trouble maker', body:'sharp tounged but golden hearted', author:'CodingChimp',id:2},
-    {title: 'My treasure', body:'How to collect all the stamps', author:'Yoshi',id:3},
-  ]);
+  const [blogs,setBlogs] = useState(null);
 
   const handleDel= (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -14,14 +10,19 @@ const Home = () => {
   }
 
   useEffect(() => {
-    console.log('use effect ran');
-    console.log(blogs);
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data)
+      })
   },[blogs])
   
   return ( 
     <div className="home">
-      <BlogList blogs={blogs.filter(blog=> blog.author == 'CodingChimp')} title="CoolChimps's" handleDel= {handleDel}/>
-      <BlogList blogs={blogs.filter(blog=> blog.author == 'Yoshi')} title="Yoshi's" handleDel= {handleDel}/>
+      {blogs && <BlogList blogs={blogs.filter(blog=> blog.author == 'CodingChimp')} title="CoolChimps's" handleDel= {handleDel}/>}
+      {blogs &&<BlogList blogs={blogs.filter(blog=> blog.author == 'Yoshi')} title="Yoshi's" handleDel= {handleDel}/>}
     </div>
    );
 }
